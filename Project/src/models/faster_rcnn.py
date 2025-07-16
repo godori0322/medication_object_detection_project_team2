101353f8d117d5c034847c90767a07aa65d56cf2
Brillight-Im <vkvntm2410@gmail.com>
2025-07-16 16:20:24 +0900

    faster rcnn파일명 변경

--- Project/src/models/faster_rcnn.py
+++ Project/src/models/faster_rcnn.py
@@ -1,9 +1,7 @@
-# src/model.py
-
 import torchvision
 from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
 
-def get_detection_model(num_classes, pretrained=True):
+def faster_rcnn(num_classes, pretrained=True):
     weights = 'FasterRCNN_ResNet50_FPN_Weights.DEFAULT' if pretrained else None
     model = torchvision.models.detection.fasterrcnn_resnet50_fpn(weights=weights)
     in_features = model.roi_heads.box_predictor.cls_score.in_features
