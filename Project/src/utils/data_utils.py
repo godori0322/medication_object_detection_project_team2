60b21446715aa4bd7e4d9f0795c596747923916a
=haru0 <haru0.ever@gmail.com>
2025-07-21 11:27:16 +0900

    change data_utils.py

--- Project/src/utils/data_utils.py
+++ Project/src/utils/data_utils.py
@@ -1,14 +1,17 @@
-
 # utils/data_utils.py
 
 import pandas as pd
 import json
+from pathlib import Path
+
 
 def load_filtered_df(cfg):
-    return pd.read_csv(cfg.filtered_annotation_csv_path)
+    csv_path = Path(cfg.base_dir) / "Project" / "data_csv" / "filtered_df.csv"
+    return pd.read_csv(csv_path)
 
 def load_mappings(cfg):
-    with open(cfg.mappings_json_path, 'r', encoding='utf-8') as f:
+    json_path = Path(cfg.base_dir) / "Project" / "data_csv" / "mappings.json"
+    with open(json_path, 'r', encoding='utf-8') as f:
         mappings = json.load(f)
 
     mappings['image_id_map'] = {
